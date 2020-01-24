8ef93037ff3c8c2070a5b06b5088bff06827bc39
Alex Kang <akang0408@gmail.com>
2020-01-24 16:38:01 -0500

    initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  devServer: {
+    // contentBase: path.join(__dirname, 'build'),
+    publicPath: '/build/',
+    // port: 8080,
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+};
