a6a8cc39a2a17b2ec3a0a1267e6710d69ec8906e
Alex Kang <akang0408@gmail.com>
2020-01-27 16:16:50 -0500

    new CSS changes

--- client/NavBar.jsx
+++ client/NavBar.jsx
@@ -3,63 +3,34 @@ import React, { Component } from 'react';
 // class to render our main app 
 class NavBar extends Component {
 
-  constructor(props) {
-    super(props)
+  render() {
+    const unitsArr = this.props.units;
+    /*
+     * Modularize adding the units 
+     */
 
-    this.state = {
-      units: [],
-      postDidMount: false,
+    const unitsLinks = [];
+
+    for (let i = 0; i < unitsArr.length; i += 1) {
+      unitsLinks.push(<li><a href={`/${unitsArr[i].unit}`} >{unitsArr[i].unit}</a></li>)
     }
 
-  }
-
-  componentDidMount() {
-
-    const unitsURL = '/units'
-
-    /* 
-    when we fetch from the unitsURL, should get an array of Objects with each object representing a different unit such as OOP or Algorithms.
-    Get the units from response.locals.units, or the response in the response from the fetch
-    
-    */
-    fetch(unitsURL)
-    .then(data => data.json())
-    .then(data => {
-      console.log('MY UNITS DATA:', data);
-      this.setState({
-        units: data,
-        postDidMount: true,
-      })
-    })
-    .catch(err => console.log('ERROR:', err));
-
-    // https://dev.to/nburgess/creating-a-react-app-with-react-router-and-an-express-backend-33l3
-
-  }
-
-
-  render() {
-
-    const unitsArr = this.state.units;
-    // conditional render 
-    if (!this.state.postDidMount) {
-      return <h1>WE LOADING BABY</h1>;
-    } 
+    console.log(unitsLinks);
 
     return (
       <div>
-        <h1 id='appTitle'>CS Study Planner</h1>
+        <h1>CS-Study</h1>
         <nav id='navBar'>
         <ul id='navbarList'>
-          <li><a id='navLinks' href={`/${unitsArr[0].unit}`} >{unitsArr[0].unit}</a></li>
-          <li><a id='navLinks' href={`/${unitsArr[1].unit}`}>{unitsArr[1].unit}</a></li>
-          <li><a id='navLinks' href={`/${unitsArr[2].unit}`}>{unitsArr[2].unit}</a></li>
-          <li><a id='navLinks' href={`/${unitsArr[3].unit}`}>{unitsArr[3].unit}</a></li>
+          {/* <li><a href={`/${unitsArr[0].unit}`} >{unitsArr[0].unit}</a></li>
+          <li><a href={`/${unitsArr[1].unit}`}>{unitsArr[1].unit}</a></li>
+          <li><a href={`/${unitsArr[2].unit}`}>{unitsArr[2].unit}</a></li>
+          <li><a href={`/${unitsArr[3].unit}`}>{unitsArr[3].unit}</a></li> */}
+          { unitsLinks }
         </ul>
         </nav>
       </div>
     )
-
   }
 }
 
