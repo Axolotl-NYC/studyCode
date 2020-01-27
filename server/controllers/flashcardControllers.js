87ec891cd31135ae7193497ac5d9818eceb179b6
Alex Kang <akang0408@gmail.com>
2020-01-27 10:30:09 -0500

    fixed so answers and questions show up in correct places in database when entered in client

--- server/controllers/flashcardControllers.js
+++ server/controllers/flashcardControllers.js
@@ -13,12 +13,12 @@ flashcardControllers.getFlashcards = (req, res, next) => {
     });
 };
 
-flashcardControllers.addedFlashcards = (req, res, next) => {
+flashcardControllers.addFlashcards = (req, res, next) => {
   console.log('this is req.body', req.body);
   console.log('this is req.params', req.params);
   const { unitId } = req.params;
   const { answer, question, created_by } = req.body;
-  const dataArr = [unitId, answer, question, created_by];
+  const dataArr = [unitId, question, answer, created_by];
   const queryString = 'INSERT INTO flashcards (unit_id, question, answer, created_by) VALUES ($1, $2, $3, $4);';
   db.query(queryString, dataArr)
     .then((response) => {
