4cb33601cd8b841af0b1fd9fcb68f199536b54ae
Matt Peters <matthew.george.peters@gmail.com>
2020-01-29 15:37:07 -0500

    turned FlashCards comp to a fx comp

--- client/components/FlashCards.jsx
+++ client/components/FlashCards.jsx
@@ -1,38 +1,23 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class FlashCard extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      question: true,
-    }
-  }
-
-  render() {
-    // we are passed in id, unit_id, question, answer
-    console.log(`flash card ${this.props.arrayId} props`, this.props)
-
-    // need a deleteCard function that is conditonal on only being able to delete if created_by is the current user
-
-    // also only display cards from the stock cards and the ones created by the user
-    return (
-      <div>
+function FlashCard (props) {
+  return (
+    <div>
       {
-        this.props.questionAnswered ?
-        <div className="flashCard_question" onClick={ () => this.props.flipFlashCard(this.props.arrayId) }>
-          <a id='questionP'>{ this.props.question }</a>
-          <button onClick={ this.props.deleteFlashCard } value={ this.props.id } className="deleteCard" type="button">x</button>
+        props.questionAnswered ?
+        <div className="flashCard_question" onClick={ () => props.flipFlashCard(props.arrayId) }>
+          <a id='questionP'>{ props.question }</a>
+          <button onClick={ props.deleteFlashCard } value={ props.id } className="deleteCard" type="button">x</button>
         </div> :
-        <div className="flashCard_question" onClick={ () => this.props.flipFlashCard(this.props.arrayId) }>
-          <a id='questionP'>{ this.props.answer }</a>
-          <button onClick={ this.props.deleteFlashCard } value={ this.props.id } className="deleteCard" type="button">x</button>
+        <div className="flashCard_question" onClick={ () => props.flipFlashCard(props.arrayId) }>
+          <a id='questionP'>{ props.answer }</a>
+          <button onClick={ props.deleteFlashCard } value={ props.id } className="deleteCard" type="button">x</button>
         </div>
       }
-      </div>
-      );
-  }
+    </div>
+  );
 }
+
 
 
 export default FlashCard;
