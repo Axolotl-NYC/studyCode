ba61e1d6be9cc3be0113ce6154540b58083b98b9
Matt Peters <matthew.george.peters@gmail.com>
2020-01-29 18:38:09 -0500

    CreateNewUnit button linked to styling, routes perfectly

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -12,7 +12,11 @@ function MainContainer (props) {
       <NavBar
         units={ props.units }
         updateCurrentUnit= { props.updateCurrentUnit } />
-      <Link to='/main-container/create-unit'>Create New Unit</Link>
+      <nav id='navBar'>
+        <ul id='navbarList'>
+          <Link to='/main-container/create-unit'>Create New Unit</Link>
+        </ul>
+      </nav>
         <Switch>
           <Route path="/main-container/units">
             { // conditional render precluded on if a NavBar selection was made, default is null.
